5f5c548204516f3d9d12c689c364bdfe7cba8d73
mariful-nyx <mariful@heliosinsider.com>
2025-01-14 10:48:53 +0600

    vercel deploy setting setup

--- bpm/settings.py
+++ bpm/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a(#j+x199*lkeh&6@par6wf!%^0v1et5tks*ml_n74r8p1yi7(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
 
 
 # Application definition
@@ -166,11 +166,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
