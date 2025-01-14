565e75dc1f6e8bf6dc881170792e7fa34f84e878
mariful-nyx <mariful@heliosinsider.com>
2025-01-14 15:02:00 +0600

    fix deploy

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput
 
